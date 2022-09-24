38a34f84a4041e4486c0e2f7b481d56820e6e00e
aboriska <aboriska@yahoo.com>
2022-09-24 17:39:27 +0300

    Initial commit

---
+++ src/01-primitives.ts
@@ -0,0 +1,30 @@
+// numbers
+let x = 10;
+// let y = 1234n
+let z = NaN;
+
+//string
+let str1: string;
+str1 = "Hello world";
+let sym1 = Symbol("as");
+
+//boolean
+let o = true;
+o = false;
+
+//nothing
+let h: undefined = undefined;
+let g: null = null;
+
+//literal
+const num = 108;
+const str2 = "str";
+
+// universal
+let mm: any = 1;
+mm = "str";
+mm = [];
+// can use only after correction
+let xx: unknown = 2;
+if (typeof xx === "string") xx.toUpperCase();
+xx = "str";
