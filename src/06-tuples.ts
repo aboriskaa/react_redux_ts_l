7b17319a271865f5faecf26d3738077f92968f37
aboriska <aboriska@yahoo.com>
2022-09-25 11:44:41 +0300

    6

---
+++ src/06-tuples.ts
@@ -0,0 +1,11 @@
+const pairs: [string, string][] = [
+	['key', 'value'],
+	['key2', 'value2'],
+];
+
+const data: [number, boolean, string] = [1, true, 'lodash'];
+
+//csv
+const doc: [string, string, number, Date][] = [];
+
+doc.push(['Boris', 'Nep', 12, new Date(1987, 3, 29)]);
